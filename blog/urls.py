fbd6c2ed4cf77913fc8a231209e44513db8e1456
Cesar <c.tabares15@hotmail.com>
2019-10-19 16:06:53 -0500

    Add logout functionality

--- blog/urls.py
+++ blog/urls.py
@@ -9,8 +9,10 @@ urlpatterns = [
     path('post/<int:pk>/edit', views.post_edit, name='post_edit'),
     path('drafts/', views.post_draft_list, name='post_draft_list'),
     path('post/<int:pk>/publish', views.post_publish, name='post_publish'),
-    #path('accounts/login/', auth_views.LoginView.as_view(template_name="registration/login.html"), name='login'),
-    path('accounts/login/', auth_views.LoginView.as_view(), name='login'), # este path, debe ir acompañado de una configuracion en settings ,
-    #para que cuando se ingresen los datos de usuario, se redireccione a la pagina de inicio 'post_list'
 
+
+    #vamos a eliminar este path de esta ruta, pq en caso que tengamos en la misma web 2 aplicaciones, el estado de login, debe ser para todas las aplicaciones
+    # no solamente blogapp
+    #path('accounts/login/', auth_views.LoginView.as_view(), name='login'), #este path, debe ir acompañado de una configuracion en settings ,
+    #para que cuando se ingresen los datos de usuario, se redireccione a la pagina de inicio 'post_list'
 ]
